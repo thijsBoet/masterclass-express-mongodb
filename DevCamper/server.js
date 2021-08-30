34d99e24d0b9240a9bfbb479b021e57cfcdfaa78
Matthijs Boet <mboet@client13.ict.fqdn>
2021-08-30 15:51:25 +0200

    setting up DB for app

---
+++ DevCamper/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+
+const bootcamps = require('./routes/bootcamps')
+
+dotenv.config({
+    path: "./config/config.env"
+})
+const app = express();
+
+// Dev logger middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
+
+// Mount router
+app.use('/api/v1/bootcamps', bootcamps)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`))
