67575d0d8a28611679b5a457c0c3cf63306ff756
Matthijs Boet <m.boet@chello.nl>
2021-08-31 13:22:32 +0200

    setup DB at Cloud Atlas and connected app

--- DevCamper/server.js
+++ DevCamper/server.js
@@ -1,13 +1,19 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
-
-const bootcamps = require('./routes/bootcamps')
+const cors = require("cors")
+const colors = require("colors")
+const connectDB = require("./config/db")
 
 dotenv.config({
     path: "./config/config.env"
 })
+
+connectDB();
+const bootcamps = require('./routes/bootcamps')
+
 const app = express();
+app.use(cors());
 
 // Dev logger middleware
 if (process.env.NODE_ENV === "development") {
@@ -19,4 +25,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`))
+const server = app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`.red)
+    server.close(() => process.exit(1))
+})
